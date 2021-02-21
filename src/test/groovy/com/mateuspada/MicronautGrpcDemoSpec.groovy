0be80ec6d879afe1e7fd54439b81741f19167c1b
Mateus Spada Leme <mateuspada@live.com>
2021-02-21 18:04:55 -0300

    initial commit

---
+++ src/test/groovy/com/mateuspada/MicronautGrpcDemoSpec.groovy
@@ -0,0 +1,19 @@
+package com.mateuspada
+
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import spock.lang.Specification
+import javax.inject.Inject
+
+@MicronautTest
+class MicronautGrpcDemoSpec extends Specification {
+
+    @Inject
+    EmbeddedApplication<?> application
+
+    void 'test it works'() {
+        expect:
+        application.running
+    }
+
+}

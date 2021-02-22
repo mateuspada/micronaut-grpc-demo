e7426756fdf0d34e29c95b799f094127bfff3df1
Mateus Spada Leme <mateuspada@live.com>
2021-02-21 23:13:48 -0300

    add: variable reading

--- src/main/kotlin/com/mateuspada/MicronautGrpcDemoEndpoint.kt
+++ src/main/kotlin/com/mateuspada/MicronautGrpcDemoEndpoint.kt
@@ -1,13 +1,16 @@
 package com.mateuspada
 
+import io.micronaut.context.annotation.Value
 import javax.inject.Singleton
 
 @Singleton
-class MicronautGrpcDemoEndpoint(private val micronautGrpcDemoService: MicronautGrpcDemoService) :
-    MicronautGrpcDemoServiceGrpcKt.MicronautGrpcDemoServiceCoroutineImplBase() {
+class MicronautGrpcDemoEndpoint(
+    private val micronautGrpcDemoService: MicronautGrpcDemoService,
+    @Value("\${message}") private val message: String
+) : MicronautGrpcDemoServiceGrpcKt.MicronautGrpcDemoServiceCoroutineImplBase() {
 
     override suspend fun getDemo(request: MicronautGrpcDemoRequest): MicronautGrpcDemoReply {
-        return micronautGrpcDemoService.getDemo(name = request.name, message = "Hello World")
+        return micronautGrpcDemoService.getDemo(name = request.name, message = message)
     }
 
 }
